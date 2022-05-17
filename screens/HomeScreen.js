cd73426b1a634b0e59992ae22b07f0476805739c
Thanongsak Sritha <mackeyrisen@gmail.com>
2022-05-17 14:34:05 +0700

    .

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import * as React from 'react';
+import {SafeAreaView,ScrollView,View,Image,Text,StyleSheet} from 'react-native';
 
 export default function HomeScreen({navigation}){
+   
+    const [products,setProducts] = React.useState([]);
+    React.useEffect(()=>{
+        fetch("https://siamcityacc.com/api/react/getProduct")
+        .then(res =>res.json())
+        .then(
+            (result)=>{
+                setProducts(result);
+            },
+            (error)=>{},
+        )
+    });
     return(
-        <View style={style.container}>
-            <Text>Home Screen</Text>
-            <Button title='Click Here' onPress={()=>alert('Button Clicked')}/>
-        </View>
+        <ScrollView>
+            {products.map(product=>(
+                <View key={product.id} style={styles.container}>
+                    <Image source={{ uri: product.img_url,}} style={styles.image}/>
+                    <Text>{product.name}</Text>
+                </View>
+            ))}
+            <View style={{height:120}}/>
+        </ScrollView>
     );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex:1,
         alignItems:'center',
-        justifyContent:'center'
+        justifyContent:'center',
+        margin:10,
+        marginLeft:40,
+        marginRight:40,
+        backgroundColor:"#fff",
+        padding:15,
+        borderRadius:15
+    },
+    image:{
+        width: 250,
+        height: 250
     }
 });
